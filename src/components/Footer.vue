d4dd936712986c577a21bf61bc704b1ecd294500
marcocnc <marcocancelliere56@gmail.com>
2023-04-28 18:32:23 +0200

    completed header and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -59,7 +59,20 @@ export default {
         </div>
     </div>
     <div class="footer-bottom">
-        <div class="container">
+        <div class="container h-100 d-flex-space-bet">
+            <div class="f-left">
+                <button>SIGN-UP NOW!</button>
+            </div>
+            <div class="f-right d-flex align-center">
+                
+                    <h3>FOLLOW US</h3>
+                    <img src="../assets/img/footer-facebook.png" alt="">
+                    <img src="../assets/img/footer-twitter.png" alt="">
+                    <img src="../assets/img/footer-youtube.png" alt="">
+                    <img src="../assets/img/footer-pinterest.png" alt="">
+                    <img src="../assets/img/footer-periscope.png" alt="">
+                
+            </div>
         </div>
     </div>
   </footer>
@@ -114,5 +127,41 @@ export default {
     .footer-bottom{
         height: 30%;
         background-color: #303030;
+    }
+
+    .f-left{
+        button{
+            background-color: #303030;
+            border: 1px solid $color_blue;
+            color: $color_white;
+            padding: 10px;
+            font-weight: bold;
+            font-size: 17px;
+            cursor: pointer;
+
+            &:active{
+                background-color: $color_blue;
+            }
+        }
+    }
+
+    .f-right{
+
+        h3{
+            padding-right: 20px;
+            color: $color_blue;
+
+            &:hover{
+                color: $color_white;
+                cursor: pointer;
+            }
+        }
+
+        img{
+            object-fit: cover;
+            vertical-align: middle;
+            margin-right: 15px;
+            cursor: pointer;
+        }
     }
 </style>
